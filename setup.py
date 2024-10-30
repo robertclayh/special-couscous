a2468e0a390ea136af657568a660611b484c0aca
Clay_Harris <robertclayh@icloud.com>
2024-10-29 20:16:10 -0400

    Demo package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='https://github.com/robertclayh/special-couscous',
+    author='R.C. Harris',
+    author_email='jbm2rt@virginia.edu',
+    license='Apache',
+    packages=['demo']
+)
